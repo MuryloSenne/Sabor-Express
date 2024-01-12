03ce39f81bf094880307949f014ca4d964ac250b
MuryloSenne <143774081+MuryloSenne@users.noreply.github.com>
2024-01-12 03:36:55 -0300

    Update app.py
    int / if

--- app.py
+++ app.py
@@ -5,5 +5,17 @@ print('2. Listar Restaurante')
 print('3. Ativar Restaurante')
 print('4. Sair\n')
 
-opcao_escolhida = input('Escolha uma opção: ')
-print(f'Voçê escolheu a opção {opcao_escolhida}!')
+opcao_escolhida = int(input('Escolha uma opção:'))
+
+
+if opcao_escolhida == 1:
+    print('Casdastrar restaurante')
+
+elif opcao_escolhida == 2:
+    print('Listar restaurante')
+
+elif opcao_escolhida == 3:
+    print('ativar restaurante')
+    
+else:
+    print('Encerrando programa')
